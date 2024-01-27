d0062144385739908671b2c55f742f7487ce954b
ReedOtto0 <153955823+ReedOtto0@users.noreply.github.com>
2024-01-26 21:55:00 -0700

    Added responsive scaling on the auto sizing display font

--- app/components/Display.js
+++ app/components/Display.js
@@ -1,7 +1,16 @@
-import { forwardRef, useImperativeHandle, useRef, useState } from "react";
-import { formatExpressionInPlace } from "./inputUtils";
-import useAutoSizingFont from "./useAutoSizingFont";
+"use client";
+
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { formatExpressionInPlace } from "../lib/inputUtils";
+import useAutoSizingFont from "../lib/useAutoSizingFont";
 import styles from "./Display.module.css";
+import useResizeObserver from "../lib/useResizeObserver";
 
 const digit = /[0-9]/;
 const mappedKeys = {
@@ -26,16 +35,57 @@ const mappedKeys = {
   Enter: "=",
 };
 
+const fontScales = {
+  sm: { input: [24, 64], output: [12, 32] },
+  md: { input: [38, 82], output: [16, 48] },
+  lg: { input: [42, 96], output: [32, 64] },
+};
+
 const Display = forwardRef(({ dispatch, calculate, result }, ref) => {
   const [value, setValue] = useState("");
-  const [autoSizeInput, resizeInput] = useAutoSizingFont(28, 88);
-  const [autoSizeOutput, resizeOutput] = useAutoSizingFont(16, 56);
+  const [fontScale, setFontScale] = useState(fontScales.md);
+  const [autoSizeInput, resizeInput] = useAutoSizingFont(...fontScale.input);
+  const [autoSizeOutput, resizeOutput] = useAutoSizingFont(...fontScale.output);
   const containerRef = useRef(null);
+  const mySize = useResizeObserver(containerRef);
+
+  useEffect(() => {
+    if (mySize.target) {
+      if (mySize.contentRect.height <= 200 && fontScale !== fontScales.sm) {
+        requestAnimationFrame(() => {
+          setFontScale(fontScales.sm);
+          resizeInput();
+          resizeOutput();
+        });
+      } else if (
+        mySize.contentRect.height <= 400 &&
+        fontScale !== fontScales.md
+      ) {
+        requestAnimationFrame(() => {
+          setFontScale(fontScales.md);
+          resizeInput();
+          resizeOutput();
+        });
+      } else if (
+        mySize.contentRect.height >= 400 &&
+        fontScale !== fontScales.lg
+      ) {
+        requestAnimationFrame(() => {
+          setFontScale(fontScales.lg);
+          resizeInput();
+          resizeOutput();
+        });
+      } else {
+        requestAnimationFrame(resizeInput);
+        requestAnimationFrame(resizeOutput);
+      }
+    }
+  }, [mySize]);
 
   useImperativeHandle(ref, () => autoSizeInput.current, []);
 
   const handleChange = (e) => {
-    setValue(e.target.value);
+    setValue(e.target.value.replace("-", "−"));
     calculate(e.target.value);
     requestAnimationFrame(resizeInput);
     requestAnimationFrame(resizeOutput);
@@ -59,6 +109,7 @@ const Display = forwardRef(({ dispatch, calculate, result }, ref) => {
 
   return (
     <div className={styles.frame} ref={containerRef}>
+      <div className="flex-grow w-full  -mt-4" />
       <input
         className={`${styles.display} ${styles.input}`}
         type="text"
@@ -74,7 +125,7 @@ const Display = forwardRef(({ dispatch, calculate, result }, ref) => {
         spellCheck="false"
       />
       <p className={`${styles.display} ${styles.result}`} ref={autoSizeOutput}>
-        {formatExpressionInPlace(result)[0]}
+        {formatExpressionInPlace(result)[0].replace("-", "−")}
       </p>
     </div>
   );
