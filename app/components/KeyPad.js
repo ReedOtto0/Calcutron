243378a9aa7bca59e4cace9187caaf0f6dbdfdd3
ReedOtto0 <153955823+ReedOtto0@users.noreply.github.com>
2024-01-26 21:56:05 -0700

    Optimized keypad rendering

--- app/components/KeyPad.js
+++ app/components/KeyPad.js
@@ -1,7 +1,9 @@
 "use client";
 
+import { memo, useEffect, useRef, useState } from "react";
 import CalcButton from "./CalcButton";
 import styles from "./KeyPad.module.css";
+import useResizeObserver from "../lib/useResizeObserver";
 
 const keyStyles = {
   AC: styles.clear,
@@ -30,23 +32,66 @@ const landscapeKeys = [
   [0, ".", "bk", "+", "="],
 ];
 
-export default function KeyPad({ dispatch }) {
+const buildGrid = (layout) => {
+  const grid = {};
+  for (let row = 0; row < layout.length; row++) {
+    for (let col = 0; col < layout.length; col++) {
+      grid[layout[row][col]] = `${row + 1} / ${col + 1} / ${row + 2} / ${
+        col + 2
+      }`;
+    }
+  }
+  return grid;
+};
+
+const KeyPad = memo(({ dispatch }) => {
+  const [keyGrid, setKeyGrid] = useState(buildGrid(portraitKeys));
+  const [orientation, setOrientation] = useState("portrait");
+  const [drawerType, setDrawerType] = useState("drawer");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const ref = useRef(null);
+  const mySize = useResizeObserver(ref);
+
+  useEffect(() => {
+    if (mySize.target) {
+      if (mySize.contentRect.width > 500) {
+        requestAnimationFrame(() => {
+          setKeyGrid(buildGrid(landscapeKeys));
+          setOrientation("landscape");
+        });
+      } else {
+        requestAnimationFrame(() => {
+          setKeyGrid(buildGrid(portraitKeys));
+          setOrientation("portrait");
+        });
+      }
+    }
+  }, [mySize]);
+
   return (
-    <div className={`${styles.keypad} ${styles.landscape}`}>
-      {landscapeKeys.flat().map((key, i) => {
-        const [row, col] = [Math.floor(i / 5) + 1, (i % 5) + 1];
-        return (
-          <CalcButton
-            label={key}
-            grid={`${row} / ${col} / ${row + 1} / ${col + 1}`}
-            click={() => {
-              dispatch(key);
-            }}
-            buttonClass={key in keyStyles ? keyStyles[key] : ""}
-            key={`${key}_button`}
-          />
-        );
-      })}
+    <div className="w-full flex flex-col items-center justify-center">
+      <div
+        className={`${styles.keypad} ${
+          orientation === "portrait" ? styles.portrait : styles.landscape
+        }`}
+        ref={ref}
+      >
+        {portraitKeys.flat().map((key) => {
+          return (
+            <CalcButton
+              label={key}
+              grid={keyGrid[key]}
+              click={() => {
+                dispatch(key);
+              }}
+              buttonClass={key in keyStyles ? keyStyles[key] : ""}
+              key={`${key}_button`}
+            />
+          );
+        })}
+      </div>
     </div>
   );
-}
+});
+
+export default KeyPad;
--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import { useCallback, useState } from "react";
 import Display from "./components/Display";
 import KeyPad from "./components/KeyPad";
 import { debounce } from "lodash";
-import { parseEquation } from "./components/calcUtils";
-import useManagedTextbox from "./components/useManagedTextbox";
+import { parseEquation } from "./lib/calcUtils";
+import useManagedTextbox from "./lib/useManagedTextbox";
+import ObserverProvider from "./lib/ObserverProvider";
 
 export default function Home() {
   const [result, setResult] = useState(" ");
@@ -25,39 +26,54 @@ export default function Home() {
     []
   );
 
-  const handleKeys = (key) => {
-    if (typeof key === "number") {
-      textbox.addText(key.toString(10));
-    } else {
-      switch (key) {
-        case "AC":
-          textbox.clear();
-          break;
-        case "bk":
-          textbox.backspace();
-          break;
-        case "=":
-          textbox.setText(parseEquation(textbox.current.value));
-          break;
-        default:
-          textbox.addText(key);
-          break;
-      }
-    }
+  const handleSubmission = () => {
+    textbox.setText(parseEquation(textbox.ref.current.value));
   };
 
+  const handleKeys = useCallback(
+    (key) => {
+      if (typeof key === "number") {
+        textbox.addText(key.toString(10));
+      } else {
+        switch (key) {
+          case "AC":
+            textbox.clear();
+            break;
+          case "bk":
+            textbox.backspace();
+            break;
+          case "=":
+            handleSubmission();
+            break;
+          default:
+            textbox.addText(key);
+            break;
+        }
+      }
+    },
+    [
+      textbox.addText,
+      textbox.clear,
+      textbox.backspace,
+      textbox.setText,
+      textbox.addText,
+    ]
+  );
+
   return (
-    <main
-      className="min-h-dvh w-full flex flex-col items-center justify-center"
-      style={{ background: "hwb(140 97% 0%)" }}
-    >
-      <Display
-        result={`${result}`}
-        dispatch={handleKeys}
-        calculate={calculate}
-        ref={textbox.ref}
-      />
-      <KeyPad dispatch={handleKeys} />
-    </main>
+    <ObserverProvider>
+      <main
+        className="min-h-dvh w-full flex flex-col items-center justify-center"
+        style={{ background: "hwb(140 97% 0%)" }}
+      >
+        <Display
+          result={`${result}`}
+          dispatch={handleKeys}
+          calculate={calculate}
+          ref={textbox.ref}
+        />
+        <KeyPad dispatch={handleKeys} />
+      </main>
+    </ObserverProvider>
   );
 }
